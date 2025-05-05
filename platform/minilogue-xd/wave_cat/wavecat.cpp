ad91ab74117a0a47efc7c1cbc1786b7963a94fff
Joe <j.m.adamson6@gmail.com>
2025-05-04 22:32:45 -0700

    begin doing wavecat stuff. swap to simpleLFO for waves

--- platform/minilogue-xd/wave_cat/wavecat.cpp
+++ platform/minilogue-xd/wave_cat/wavecat.cpp
@@ -39,18 +39,24 @@
  */
 
 #include "userosc.h"
+#include "simplelfo.hpp"
 #include "wavecat.hpp"
 
-
-static Waves s_waves;
-static float hi_phi;
+static float phi, w0;
+static float shape;
+static dsp::SimpleLFO osc;
 
 typedef __uint32_t uint32_t;
+
+static dsp::BiQuad prelpf;
+static dsp::BiQuad postlpf;
 
 void OSC_INIT(uint32_t platform, uint32_t api)
 {
   (void)platform;
   (void)api;
+  prelpf.mCoeffs.setPoleLP(0.8f);
+  postlpf.mCoeffs.setFOLP(osc_tanpif(0.45f));
 }
 
 void OSC_CYCLE(const user_osc_param_t *const params,
@@ -58,82 +64,29 @@ void OSC_CYCLE(const user_osc_param_t *const params,
                const uint32_t frames)
 {
 
-  Waves::State &s = s_waves.state;
-  const Waves::Params &p = s_waves.params;
-
-  // Handle events.
-  {
-    const uint32_t flags = s.flags;
-    s.flags = Waves::k_flags_none;
-
-    s_waves.updatePitch(osc_w0f_for_note((params->pitch) >> 8, params->pitch & 0xFF));
-
-    s_waves.updateWaves(flags);
-
-    if (flags & Waves::k_flag_reset)
-      s.reset();
-
-    s.lfo = q31_to_f32(params->shape_lfo);
-
-    if (flags & Waves::k_flag_bitcrush)
-    {
-      s.dither = p.bitcrush * 2e-008f;
-      s.bitres = osc_bitresf(p.bitcrush);
-      s.bitresrcp = 1.f / s.bitres;
-    }
-  }
-
-  // Temporaries.
-  float phi0 = s.phi0;
-  float phi1 = s.phi1;
-  float phisub = s.phisub;
-
-  float lfoz = s.lfoz;
-  const float lfo_inc = (s.lfo - lfoz) / frames;
-
-  const float ditheramt = p.bitcrush * 2e-008f;
-
-  const float bitres = osc_bitresf(p.bitcrush);
-  const float bitres_recip = 1.f / bitres;
-
-  const float submix = p.submix;
-  const float ringmix = p.ringmix;
-
-  dsp::BiQuad &prelpf = s_waves.prelpf;
-  dsp::BiQuad &postlpf = s_waves.postlpf;
-
-
-  
   q31_t *__restrict y = (q31_t *)yn;
   const q31_t *y_e = y + frames;
 
-  for (; y < y_e;)
+  while (y < y_e)
   {
     // where in the wave to split
-    const float divider = clipminmaxf(0.f, p.shape + lfoz, 1.f); // shape blend
+    const float divider = clipminmaxf(0.f, shape, 1.f); // shape blend
 
     float sig;
-    if (hi_phi <= divider)
+    if (phi <= divider)
     {
-      sig = osc_wave_scanf(s.wave0, 2 * phi0);
+      // sig = osc_wave_scanf(s.wave0, 2 * phi0);
+      sig = osc.sine_bi();
     }
     else
     {
-      sig = 0; // osc_wave_scanf(s.wave1, phi1);
+      //sig = 0; // osc_wave_scanf(s.wave1, phi1);
+      sig = osc.triangle_bi();
     }
-
-    // sig = (1.f - wavemix) * osc_wave_scanf(s.wave0, phi0);
-    // sig += wavemix * osc_wave_scanf(s.wave1, phi1);
-
-    // const float subsig = osc_wave_scanf(s.subwave, phisub);
-    // sig = (1.f - submix) * sig + submix * subsig;
-    // sig = (1.f - ringmix) * sig + ringmix * (subsig * sig);
 
     sig = clip1m1f(sig);
 
     sig = prelpf.process_fo(sig);
-    // sig += s.dither * osc_white();
-    sig = si_roundf(sig * s.bitres) * s.bitresrcp;
     sig = postlpf.process_fo(sig);
     sig = osc_softclipf(0.125f, sig);
 
@@ -141,31 +94,18 @@ void OSC_CYCLE(const user_osc_param_t *const params,
 
     // modulo action here
 
-    hi_phi += s.w00;
-    hi_phi -= (uint32_t)hi_phi;
+    phi += w0;
+    phi -= (uint32_t)phi;
 
-    phi0 += s.w00;
-    phi0 -= (uint32_t)phi0;
-
-    phi1 += s.w01;
-    phi1 -= (uint32_t)phi1;
-
-    phisub += s.w0sub;
-    phisub -= (uint32_t)phisub;
-
-    lfoz += lfo_inc;
+    osc.cycle();
   }
-
-  s.phi0 = phi0;
-  s.phi1 = phi1;
-  s.phisub = phisub;
-  s.lfoz = lfoz;
 }
 
 void OSC_NOTEON(const user_osc_param_t *const params)
 {
-  (void)params;
-  s_waves.state.flags |= Waves::k_flag_reset;
+  osc.reset();
+  w0 = osc_w0f_for_note((params->pitch) >> 8, params->pitch & 0xFF);
+  osc.setW0(w0);
 }
 
 void OSC_NOTEOFF(const user_osc_param_t *const params)
@@ -175,65 +115,34 @@ void OSC_NOTEOFF(const user_osc_param_t *const params)
 
 void OSC_PARAM(uint16_t index, uint16_t value)
 {
-  Waves::Params &p = s_waves.params;
-  Waves::State &s = s_waves.state;
-
   switch (index)
   {
   case k_user_osc_param_id1:
-    // wave 0
-    // select parameter
-    {
-      static const uint8_t cnt = k_waves_a_cnt + k_waves_b_cnt + k_waves_c_cnt;
-      p.wave0 = value % cnt;
-      s.flags |= Waves::k_flag_wave0;
-    }
     break;
 
   case k_user_osc_param_id2:
-    // wave 1
-    // select parameter
-    {
-      static const uint8_t cnt = k_waves_d_cnt + k_waves_e_cnt + k_waves_f_cnt;
-      p.wave1 = value % cnt;
-      s.flags |= Waves::k_flag_wave1;
-    }
     break;
 
   case k_user_osc_param_id3:
-    // sub wave
-    // select parameter
-    p.subwave = value % k_waves_a_cnt;
-    s.flags |= Waves::k_flag_subwave;
     break;
 
   case k_user_osc_param_id4:
-    // sub mix
-    // percent parameter
-    p.submix = clip01f(0.05f + value * 0.01f * 0.90f); // scale in 0.05 - 0.95
     break;
 
   case k_user_osc_param_id5:
-    // ring mix
-    // percent parameter
-    p.ringmix = clip01f(value * 0.01f);
     break;
 
   case k_user_osc_param_id6:
-    // bit crush
-    // percent parameter
-    p.bitcrush = clip01f(value * 0.01f);
-    s.flags |= Waves::k_flag_bitcrush;
     break;
 
   case k_user_osc_param_shape:
     // 10bit parameter
-    p.shape = param_val_to_f32(value);
+    shape = param_val_to_f32(value);
     break;
 
   case k_user_osc_param_shiftshape:
-    // 10bit parameter
-    p.shiftshape = 1.f + param_val_to_f32(value);
+    //  // 10bit parameter
+    //  p.shiftshape = 1.f + param_val_to_f32(value);
     break;
 
   default:
